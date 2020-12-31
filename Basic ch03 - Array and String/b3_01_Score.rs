ee61610e8ebec4a9694fd600d4bcae44487807b7
fanghao.xin <fanghao_xin@outlook.com>
2020-12-31 21:09:44 +0800

    fold练习

--- Basic ch03 - Array and String/b3_01_Score.rs
+++ Basic ch03 - Array and String/b3_01_Score.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(),Box<dyn Error>> {
     for line in io::stdin().lock().lines().map(|x| x.unwrap()).skip(1) {
         let score = line.chars().fold((0,0),|mut s,x| {
             s.0 += if x=='O' {s.1+=1;s.1} else {s.1=0;0};
-            (s.0,s.1)
+            s
         }).0;
         println!("{}",score);
     }
